0308d6683363cd3964851b2e43bd7d05b6b0582b
Andika Bahari <kzlfxx@gmail.com>
2022-05-16 19:28:38 +0700

    Add multer to handle file request

--- src/router.js
+++ src/router.js
@@ -1,4 +1,7 @@
 const router = require("express").Router();
+const multer = require("multer");
+
+const upload = multer();
 
 const homeController = require("./controllers/home.controller");
 const authController = require("./controllers/v1/auth.controller");
@@ -10,6 +13,25 @@ router.post("/v1/auth/login", authController.login);
 router.post("/v1/auth/register", authController.register);
 router.get("/v1/users/:id", userController.getUser);
 router.put("/v1/users/:id", userController.updateUser);
-router.post("/v1/foods/prediction", foodController.predict);
+router.post(
+  "/v1/foods/prediction",
+  upload.single("image"),
+  foodController.predict
+);
+
+/**
+ * FOR TESTING PURPOSE ONLY
+ */
+router.get("/upload", (req, res) => {
+  res
+    .writeHead(200, { "Content-Type": "text/html" })
+    .end(
+      "<!DOCTYPE HTML><html><body>" +
+        "<form method='post' action='/v1/foods/prediction' enctype='multipart/form-data'>" +
+        "<input type='file' name='image'/>" +
+        "<input type='submit' /></form>" +
+        "</body></html>"
+    );
+});
 
 module.exports = router;
