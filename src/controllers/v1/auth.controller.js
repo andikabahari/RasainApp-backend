44f9591e31cd01f675bf0301189be879b08e5b02
Andika Bahari <kzlfxx@gmail.com>
2022-05-21 16:22:10 +0700

    Update auth response format

--- src/controllers/v1/auth.controller.js
+++ src/controllers/v1/auth.controller.js
@@ -3,16 +3,22 @@ const handleAsync = require("../../utils/handleAsync");
 const login = handleAsync(async (req, res) => {
   res.json({
     error: false,
-    message: "You have logged in successfully",
-    token: "thisismytoken",
+    message: "You are successfully logged in",
+    data: {
+      user: {
+        userId: 69,
+        fullName: "Allan Holdsworth",
+        email: "allan@example.com",
+      },
+      token: "thisismytoken",
+    },
   });
 });
 
 const register = handleAsync(async (req, res) => {
   res.json({
     error: false,
-    message: "New account created",
-    token: "thisismytoken",
+    message: "You have successfully registered",
   });
 });
 
