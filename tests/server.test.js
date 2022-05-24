3bebd098bd9d971980cf08a2e74edbf61cba6336
Andika Bahari <kzlfxx@gmail.com>
2022-05-24 10:31:51 +0700

    Add test cases

--- tests/server.test.js
+++ tests/server.test.js
@@ -1,12 +1,86 @@
 const supertest = require("supertest");
+const httpStatus = require("http-status");
 const app = require("../src/app");
 
 const request = supertest(app);
 
-it("Call the / endpoint", async () => {
-  const res = await request.get("/");
-  expect(res.status).toBe(200);
-  expect(typeof res.body).toBe("object");
-  expect(res.body).toHaveProperty("error");
-  expect(res.body).toHaveProperty("message");
+it("GET /", async () => {
+  try {
+    await request
+      .get("/")
+      .expect(httpStatus.OK)
+      .expect("Content-Type", /json/)
+      .field("error", false);
+  } catch (err) {
+    throw err;
+  }
+});
+
+it("POST /v1/auth/login", async () => {
+  try {
+    const body = {
+      email: "testing@example.com",
+      password: "testing!w43b9m",
+    };
+    await request
+      .post("/v1/auth/login")
+      .attach(body)
+      .expect(httpStatus.OK)
+      .expect("Content-Type", /json/)
+      .field("error", false);
+  } catch (err) {
+    throw err;
+  }
+});
+
+it("POST /v1/auth/register", async () => {
+  try {
+    const body = {
+      fullName: "Testing",
+      email: "testing@example.com",
+      password: "testing!w43b9m",
+    };
+    await request
+      .post("/v1/auth/register")
+      .attach(body)
+      .expect(httpStatus.OK)
+      .expect("Content-Type", /json/)
+      .field("error", true);
+  } catch (err) {
+    throw err;
+  }
+});
+
+it("GET /v1/users/:id", async () => {
+  try {
+    const userId = 1;
+    await request
+      .get(`/v1/users/${userId}`)
+      .expect(httpStatus.OK)
+      .expect("Content-Type", /json/)
+      .field("error", false);
+  } catch (err) {
+    throw err;
+  }
+});
+
+it("PUT /v1/users/:id", async () => {
+  try {
+    const userId = 1;
+    const token = "randomtokenstring";
+    const body = {
+      fullName: "Testing",
+      email: "testing@example.com",
+      password: "testing!w43b9m",
+    };
+    await request
+      .put(`/v1/users/${userId}`)
+      .set("Authorization", `Bearer ${token}`)
+      .attach(body)
+      .expect(httpStatus.OK)
+      .expect("Content-Type", /json/)
+      .field("error", false);
+  } catch (err) {
+    throw err;
+  }
 });
