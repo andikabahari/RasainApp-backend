9119822d3c993256d1318aa9d1774912c0bdc46b
Andika Bahari <kzlfxx@gmail.com>
2022-05-24 11:55:05 +0700

    Add login feature

--- src/controllers/v1/auth.controller.js
+++ src/controllers/v1/auth.controller.js
@@ -1,23 +1,53 @@
 const httpStatus = require("http-status");
 const moment = require("moment");
+const bcrypt = require("bcrypt");
 const handleAsync = require("../../utils/handleAsync");
 const db = require("../../utils/db");
 const hash = require("../../utils/hash");
+const generateToken = require("../../utils/generateToken");
 const ApiError = require("../../utils/ApiError");
 
 const login = handleAsync(async (req, res) => {
-  res.json({
-    error: false,
-    message: "Login successful",
-    data: {
-      user: {
-        userId: 69,
-        fullName: "Allan Holdsworth",
-        email: "allan@example.com",
+  const { email, password } = req.body;
+
+  try {
+    const usersRef = await db.collection("users");
+
+    const usersRes = await usersRef.where("email", "==", email).get();
+    if (usersRes.size === 0) {
+      return res.json({
+        error: true,
+        message: "Email is not registered",
+      });
+    }
+
+    const user = usersRes.docs[0].data();
+    const isValid = await bcrypt.compare(password, user.password);
+    if (!isValid) {
+      return res.json({
+        error: true,
+        message: "Password is incorrect",
+      });
+    }
+
+    const userId = usersRes.docs[0].id;
+    const token = generateToken({ userId });
+    res.json({
+      error: false,
+      message: "Login successful",
+      data: {
+        user: {
+          userId,
+          fullName: user.fullName,
+          email: user.email,
+        },
+        token,
       },
-      token: "thisismytoken",
-    },
-  });
+    });
+  } catch (err) {
+    console.error(err);
+    throw new ApiError(httpStatus.INTERNAL_SERVER_ERROR, "Server error");
+  }
 });
 
 const register = handleAsync(async (req, res) => {
@@ -26,11 +56,11 @@ const register = handleAsync(async (req, res) => {
   try {
     const usersRef = await db.collection("users");
 
-    const users = await usersRef.where("email", "==", email).get();
-    if (users.size) {
+    const usersRes = await usersRef.where("email", "==", email).get();
+    if (usersRes.size > 0) {
       return res.json({
         error: true,
-        message: "Email taken",
+        message: "Email already taken",
       });
     }
 
