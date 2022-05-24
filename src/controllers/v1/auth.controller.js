8a3ac86124c53b173ae6e024a07fb59daa5e2f7b
Andika Bahari <kzlfxx@gmail.com>
2022-05-24 11:20:58 +0700

    Add registration feature

--- src/controllers/v1/auth.controller.js
+++ src/controllers/v1/auth.controller.js
@@ -1,9 +1,14 @@
+const httpStatus = require("http-status");
+const moment = require("moment");
 const handleAsync = require("../../utils/handleAsync");
+const db = require("../../utils/db");
+const hash = require("../../utils/hash");
+const ApiError = require("../../utils/ApiError");
 
 const login = handleAsync(async (req, res) => {
   res.json({
     error: false,
-    message: "You are successfully logged in",
+    message: "Login successful",
     data: {
       user: {
         userId: 69,
@@ -16,10 +21,34 @@ const login = handleAsync(async (req, res) => {
 });
 
 const register = handleAsync(async (req, res) => {
-  res.json({
-    error: false,
-    message: "You have successfully registered",
-  });
+  const { fullName, email, password } = req.body;
+
+  try {
+    const usersRef = await db.collection("users");
+
+    const users = await usersRef.where("email", "==", email).get();
+    if (users.size) {
+      return res.json({
+        error: true,
+        message: "Email taken",
+      });
+    }
+
+    await usersRef.add({
+      fullName,
+      email,
+      password: hash(password),
+      createdAt: moment(new Date()).format("YYYY-MM-DD HH:mm:ss"),
+    });
+
+    res.json({
+      error: false,
+      message: "Registration successful",
+    });
+  } catch (err) {
+    console.error(err);
+    throw new ApiError(httpStatus.INTERNAL_SERVER_ERROR, "Server error");
+  }
 });
 
 module.exports = { login, register };
