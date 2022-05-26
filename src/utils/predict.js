8fcbaacdb7d91aa07874925dbc2b18b922d3c157
Andika Bahari <kzlfxx@gmail.com>
2022-05-26 14:45:09 +0700

    Update predict util

--- src/utils/predict.js
+++ src/utils/predict.js
@@ -1,10 +1,12 @@
 const tf = require("@tensorflow/tfjs-node");
 
 const predict = async (image) => {
-  const model = await tf.loadLayersModel(
-    `file://${process.env.TENSORFLOW_SAVED_MODEL}`
-  );
-  // return model.predict(input);
+  image = tf.node.decodeImage(image);
+  image = tf.expandDims(image);
+  const path = `file://${process.env.TENSORFLOW_SAVED_MODEL}`;
+  const model = await tf.loadLayersModel(path);
+  const result = model.predict(image);
+  return result.data();
 };
 
 module.exports = predict;
