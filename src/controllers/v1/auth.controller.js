ec3445c982ae71194e6acd8661e24cd6874c22ac
Andika Bahari <kzlfxx@gmail.com>
2022-05-26 16:17:31 +0700

    Move validation to middleware

--- src/controllers/v1/auth.controller.js
+++ src/controllers/v1/auth.controller.js
@@ -5,91 +5,67 @@ const handleAsync = require("../../utils/handleAsync");
 const db = require("../../utils/db");
 const hash = require("../../utils/hash");
 const generateToken = require("../../utils/generateToken");
-const ApiError = require("../../utils/ApiError");
-const authValidation = require("../../validations/auth.validation");
 
 const login = handleAsync(async (req, res) => {
-  try {
-    await authValidation.login.validateAsync(req.body);
-  } catch (err) {
-    throw new ApiError(httpStatus.BAD_REQUEST, err.message);
-  }
+  const { email, password } = req.value;
 
-  try {
-    const { email, password } = req.body;
-
-    const usersRef = await db.collection("users");
-    const usersRes = await usersRef.where("email", "==", email).get();
-    if (usersRes.size === 0) {
-      return res.json({
-        error: true,
-        message: "Email is not registered",
-      });
-    }
-
-    const user = usersRes.docs[0].data();
-    const isValid = await bcrypt.compare(password, user.password);
-    if (!isValid) {
-      return res.json({
-        error: true,
-        message: "Password is incorrect",
-      });
-    }
-
-    const userId = usersRes.docs[0].id;
-    const token = generateToken({ userId });
-    res.json({
-      error: false,
-      message: "Login successful",
-      data: {
-        user: {
-          userId,
-          fullName: user.fullName,
-          email: user.email,
-        },
-        token,
-      },
+  const usersRef = await db.collection("users");
+  const usersRes = await usersRef.where("email", "==", email).get();
+  if (usersRes.size === 0) {
+    return res.json({
+      error: true,
+      message: "Email is not registered",
     });
-  } catch (err) {
-    console.error(err);
-    throw new ApiError(httpStatus.INTERNAL_SERVER_ERROR, "Server error");
   }
+
+  const user = usersRes.docs[0].data();
+  const isValid = await bcrypt.compare(password, user.password);
+  if (!isValid) {
+    return res.json({
+      error: true,
+      message: "Password is incorrect",
+    });
+  }
+
+  const userId = usersRes.docs[0].id;
+  const token = generateToken({ userId });
+  res.json({
+    error: false,
+    message: "Login successful",
+    data: {
+      user: {
+        userId,
+        fullName: user.fullName,
+        email: user.email,
+      },
+      token,
+    },
+  });
 });
 
 const register = handleAsync(async (req, res) => {
-  try {
-    await authValidation.register.validateAsync(req.body);
-  } catch (err) {
-    throw new ApiError(httpStatus.BAD_REQUEST, err.message);
+  const { fullName, email, password } = req.value;
+
+  const usersRef = await db.collection("users");
+  const usersRes = await usersRef.where("email", "==", email).get();
+  if (usersRes.size > 0) {
+    return res.json({
+      error: true,
+      message: "Email is already taken",
+    });
   }
 
-  try {
-    const { fullName, email, password } = req.body;
+  await usersRef.add({
+    fullName,
+    email,
+    password: hash(password),
+    createdDate: moment(new Date()).format("YYYY-MM-DD"),
+  });
 
-    const usersRef = await db.collection("users");
-    const usersRes = await usersRef.where("email", "==", email).get();
-    if (usersRes.size > 0) {
-      return res.json({
-        error: true,
-        message: "Email is already taken",
-      });
-    }
-
-    await usersRef.add({
-      fullName,
-      email,
-      password: hash(password),
-      createdDate: moment(new Date()).format("YYYY-MM-DD"),
-    });
-
-    res.json({
-      error: false,
-      message: "Registration successful",
-    });
-  } catch (err) {
-    console.error(err);
-    throw new ApiError(httpStatus.INTERNAL_SERVER_ERROR, "Server error");
-  }
+  res.json({
+    error: false,
+    message: "Registration successful",
+  });
 });
 
 module.exports = { login, register };
--- src/router.js
+++ src/router.js
@@ -4,6 +4,9 @@ const multer = require("multer");
 const upload = multer();
 
 const auth = require("./middlewares/auth");
+const validate = require("./middlewares/validate");
+
+const authValidation = require("./validations/auth.validation");
 
 const homeController = require("./controllers/home.controller");
 const authController = require("./controllers/v1/auth.controller");
@@ -11,8 +14,16 @@ const userController = require("./controllers/v1/user.controller");
 const predictionController = require("./controllers/v1/prediction.controller");
 
 router.get("/", homeController.index);
-router.post("/v1/auth/login", authController.login);
-router.post("/v1/auth/register", authController.register);
+router.post(
+  "/v1/auth/login",
+  validate(authValidation.login),
+  authController.login
+);
+router.post(
+  "/v1/auth/register",
+  validate(authValidation.register),
+  authController.register
+);
 router.get("/v1/users/:id", userController.getUser);
 router.put("/v1/users/:id", auth, userController.updateUser);
 router.post(
