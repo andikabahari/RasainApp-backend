50d52571bca802b5ff14ed4fc6c00151a4ab57da
Andika Bahari <kzlfxx@gmail.com>
2022-05-26 14:46:52 +0700

    Update prediction controller

--- src/controllers/v1/prediction.controller.js
+++ src/controllers/v1/prediction.controller.js
@@ -1,13 +1,40 @@
+const httpStatus = require("http-status");
+const sharp = require("sharp");
 const handleAsync = require("../../utils/handleAsync");
+const predict = require("../../utils/predict");
+const ApiError = require("../../utils/ApiError");
+
+const labels = [
+  "bakso",
+  "klepon",
+  "nasi-goreng",
+  "onde-onde",
+  "pastel",
+  "rendang",
+  "sate",
+  "tahu-petis",
+];
 
 const getPrediction = handleAsync(async (req, res) => {
-  res.json({
-    error: false,
-    message: "Image uploaded",
-    data: {
-      prediction: {},
-    },
-  });
+  try {
+    const imageBuf = await sharp(req.file.buffer).resize(150, 150).toBuffer();
+    const imageUint = Uint8Array.from(imageBuf);
+    const prediction = await predict(imageUint);
+
+    let predictionWithLabels = {};
+    for (let i = 0; i < prediction.length; i++) {
+      predictionWithLabels[labels[i]] = prediction[i];
+    }
+
+    res.json({
+      error: false,
+      message: "Prediction successful",
+      data: { prediction: predictionWithLabels },
+    });
+  } catch (err) {
+    console.error(err);
+    throw new ApiError(httpStatus.INTERNAL_SERVER_ERROR, "Server error");
+  }
 });
 
 module.exports = { getPrediction };
